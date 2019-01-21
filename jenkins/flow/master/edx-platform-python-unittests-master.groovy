ccef186b55493a77251af79db11c93a4f4e36b16
sambapete <pierre.mailhot@gmail.com>
2019-01-21 14:03:52 -0500

    removing ginkgo references

--- jenkins/flow/master/edx-platform-python-unittests-master.groovy
+++ jenkins/flow/master/edx-platform-python-unittests-master.groovy
@@ -26,7 +26,7 @@ try{
     new StringParameterValue("BUILD_STATUS", "pending"),
     new StringParameterValue("TARGET_URL", jobUrl),
     new StringParameterValue("DESCRIPTION", "Pending"),
-    new StringParameterValue("CONTEXT", "jenkins/ginkgo/python"),
+    new StringParameterValue("CONTEXT", "jenkins/python"),
   ]
 
   def statusJob = Hudson.instance.getJob('github-build-status')
